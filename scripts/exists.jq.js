6f33c2527ba5bff7dd77cd23cff8c0d8a6b81112
Timothy Totten <2009@huri.net>
2012-03-07 18:12:17 -0800

    Added exists jquery plugin.

---
+++ scripts/exists.jq.js
@@ -0,0 +1,13 @@
+/*
+ * A jQuery plugin that adds an exists() method to selectors, so you can
+ * see if an element exists.
+ */
+
+(function($) 
+{
+  $.fn.exists = function ()
+  {
+    return this.length !== 0;
+  };
+})(jQuery);
+
