8936fb90496383891847a00effac06cd2fb05be1
Timothy Totten <2010@huri.net>
2013-06-11 12:59:12 -0700

    oQuery 2.0, as new API, with backwards compatible wrapper.

--- scripts/oquery.js
+++ scripts/oquery.js
@@ -1,11 +1,35 @@
-// Object Queries 
-
-var oQuery = {};
-
-oQuery.find = function (query, objarr, single, returnindex)
+/**
+ * oQuery, searches for objects within an array matching certain
+ * property values. This is the new API, which takes named options.
+ *
+ *  single   if true, we return the first matching object.
+ *  index    if true, we return the index position in the array.
+ *
+ */
+var oQuery = function (query, objarr, opts)
 {
-//  console.log("we're in oQuery.find()");
+//  console.log("we're in oQuery()");
+
   var matched = [];
+
+  if (opts === undefined || opts === null)
+  {
+    opts = {};
+  }
+
+  if (typeof query != 'object')
+  {
+    if (opts.single === true)
+    { // The query will be matched against the 'id' property.
+      query = { id: query };
+    }
+    else
+    {
+      console.log("Invalid query passed to oQuery()");
+      return matched;
+    }
+  }
+
   for (var i in objarr)
   {
 //    console.log("iterating item #"+i);
@@ -23,10 +47,10 @@ oQuery.find = function (query, objarr, single, returnindex)
     if (match) 
     {
 //      console.log("we found a match");
-      if (single)
+      if (opts.single === true)
       {
 //        console.log("returning the single item");
-        if (returnindex)
+        if (opts.index === true)
         {
           return i;
         }
@@ -35,7 +59,7 @@ oQuery.find = function (query, objarr, single, returnindex)
           return item;
         }
       }
-      if (returnindex)
+      if (opts.index === true)
       {
         matched.push(i);
       }
@@ -45,8 +69,42 @@ oQuery.find = function (query, objarr, single, returnindex)
       }
     }
   }
-  if (single)
+
+  if (opts.single === true)
     return null;
+
   return matched;
+}
+
+/**
+ * Compatibility wrapper for the older API.
+ */
+oQuery.find = function (query, objarray, single, returnindex)
+{
+  return oQuery(query, objarray, {single: single, index: returnindex});
+}
+
+/**
+ * Get a single object from a container.
+ */
+oQuery.get = function (query, objarray)
+{
+  return oQuery(query, objarray, {single: true});
+}
+
+/**
+ * Get the index of a single object in a container.
+ */
+oQuery.pos = function (query, objarray)
+{
+  return oQuery(query, objarray, {single: true, index: true});
+}
+
+/**
+ * Get the index of multiple objects in a container.
+ */
+oQuery.indexes = function (query, objarray)
+{
+  return oQuery(query, objarray, {index: true});
 }
 
