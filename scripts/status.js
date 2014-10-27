87567578f132bce8fca94185e8d744dd866d69c6
Timothy Totten <2010@huri.net>
2014-10-27 15:54:05 -0700

    Added Status library, works like the Nano.php version.

---
+++ scripts/status.js
@@ -0,0 +1,114 @@
+/**
+ * Status messages, compatible with Nano.php's message format.
+ *
+ * Requires:
+ *
+ * #common
+ * vsprintf
+ *
+ * TODO: replace hard coded HTML with riot template support.
+ * TODO: support 'vars' like PHP version.
+ */
+ 
+(function (root, $)
+{
+  "use strict";
+  
+  if (root.Nano === undefined)
+    root.Nano = {};
+  
+  Nano.Status = function (options)
+  {
+    if (options === undefined || options === null)
+      options = {};
+  
+    var msgs = options.msgElement !== undefined 
+        ? options.msgElement 
+        : '#status_messages';
+  
+    this.messages = $(msgs).JSON();
+ 
+    this.alerts = options.uiElement !== undefined
+        ? options.uiElement
+        : '#alerts';
+
+    var default_symbols = {"error":"!", "warning":"?", "message":"*"};
+  
+    this.symbols = options.symbols !== undefined
+        ? options.symbols
+        : default_symbols;
+  }
+  
+  Nano.Status.prototype.show = function (type, message, tag)
+  {
+    var html = '<div class="news '+tag+'">'
+      + '<span class="'+type+'">'+this.symbols[type]+'</span>'
+      + '<span class="statusmessage">'+message+'</span>'
+      + '</div>';
+    $(this.alerts).show().append(html);
+  }
+  
+  Nano.Status.prototype.get = function (msgid, reps)
+  {
+    var msgtext = this.messages[msgid];
+    if ($.isArray(reps))
+    {
+      msgtext = vsprintf(msgtext, reps);
+    }
+    else if ($.isPlainObject(reps))
+    {
+      for (var rkey in reps)
+      {
+        msgtext = msgtext.replace(rkey, reps[rkey])
+      }
+    }
+    return msgtext;
+  }
+  
+  Nano.Status.prototype.clear = function (tag)
+  {
+    $(this.alerts+' div.'+tag).remove();
+    if ($(this.alerts+' div').length == 0)
+    {
+      $(this.alerts).hide();
+    }
+  }
+  
+  Nano.Status.prototype.msg = function (msgid, tag, reps, type)
+  {
+    if (type === undefined)
+      type = 'message';
+
+    if ($.isPlainObject(msgid))
+    {
+      if ('args' in msgid)
+        reps = msgid.args;
+      else if ('vars' in msgid)
+        reps = msgid.vars;
+
+      if ('key' in msgid)
+        msgid = msgid.key;
+      else
+        return console.log("msg() message missing 'key'.");
+    }
+    else if ($.isArray(msgid))
+    {
+      reps  = msgid.slice(1);
+      msgid = msgid[0];
+    }
+
+    var message = this.get(msgid, reps);
+    this.show(type, message, tag);
+  }
+  
+  Nano.Status.prototype.warn = function (msgid, tag, reps)
+  {
+    this.msg(msgid, tag, reps, 'warning');
+  }
+  
+  Nano.Status.prototype.err = function (msgid, tag, reps)
+  {
+    this.msg(msgid, tag, reps, 'error');
+  }
+ 
+})(window, jQuery);
