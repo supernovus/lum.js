e9ddf197b790b3c948a63c03288874f7dcb71ca9
Timothy Totten <2010@huri.net>
2015-07-15 12:10:04 -0700

    fixes

--- grunt/riot.js
+++ grunt/riot.js
@@ -1,21 +1,19 @@
 /**
  * Use if you have Riot.js templates to compile.
- *
- * The default setup assumes ES6 scripting in the templates.
  */
 module.exports = function (grunt, options)
 {
   return {
     options:
     {
-      type:    'es6',
+//      type:    'es6',
       modular: 
       {
         type: 'amd',
         deps:
         [
-          {'ext/riot-core': 'riot'},
-          {'ext/jquery':    '$'},
+          'riot',
+          {'jquery': '$'},
         ],
       }
     },
