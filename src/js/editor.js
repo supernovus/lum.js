3f8a90bba27c98bd2cb244b932718047b2145638
Timothy Totten <2010@huri.net>
2015-10-27 11:32:21 -0700

    Ported Editor from v1.5 to ES6 syntax.

---
+++ src/js/editor.js
@@ -0,0 +1,175 @@
+/**
+ * Editor framework
+ *
+ * Makes web based code editors easy to use.
+ * Currently using Ace as it's backend component.
+ */
+
+"use strict";
+
+import $ from 'jquery';
+import ace from 'ace/src-min-noconflict/ace';
+import CryptoJS from 'crypto/components/core-min';
+import 'crypto/components/enc-base64-min';
+
+export default class Editor
+{
+  /**
+   * Build an editor object.
+   */ 
+  constructor(opts)
+  {
+    this.lang = 'lang' in opts ? opts.lang : 'xml';
+    this.srcType = 'sourceEncoding' ? opts.sourceEncoding : false;
+    this.srcElement = 'sourceElement' in opts ? opts.sourceElement : null;
+    this.srcUrl = 'sourceUrl' in opts ? opts.sourceUrl : null;
+    this.editElement = 'editElement' in opts ? opts.editElement : 'editor';
+    this.saveMethod = 'saveMethod' in opts ? opts.saveMethod : 'PUT';
+  }
+
+  getEditor()
+  {
+    if (this.editor === undefined)
+    {
+      this.editor = ace.edit(this.editElement);
+      if (this.lang)
+        this.editor.getSession().setMode("ace/mode/"+this.lang);
+
+    }
+    return this.editor;
+  }
+
+  setLang(lang)
+  {
+    this.getEditor().getSession().setMode("ace/mode/"+lang);
+    this.lang = lang;
+  }
+
+  load(data)
+  {
+    var dtype = typeof data;
+
+    if (dtype === "string")
+    { // The data is from a string.
+      this.setData(data);
+    }
+    else if (dtype === "function")
+    { // The data is from a function.
+      this.setData(data(this));
+    }
+    else if (this.srcElement !== null)
+    { // The data is from an element.
+      data = $(this.srcElement).val();
+      this.setData(data);
+    }
+    else if (this.srcUrl !== null)
+    { // The data is from a URL.
+      var self = this;
+      $.get(this.srcUrl, function (data, textStatus, jq)
+      {
+        self.setData(data);
+      }); 
+    }
+    else
+    { // Unknown data.
+      return false;
+    }
+
+    return true;
+  }
+
+  setData(data)
+  {
+    var editor = this.getEditor();
+    if (this.srcType === Editor.BASE64)
+    {
+//      console.log("decoding base64: "+data);
+      data = CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+//      console.log("decoded as: "+data);
+    }
+    editor.setValue(data);
+  }
+
+  loadUrl(url)
+  {
+    this.srcElement = null;
+    this.srcUrl = url;
+    return this.load();
+  }
+
+  loadTag(tag)
+  {
+    this.srcElement = tag;
+    this.srcUrl = null;
+    return this.load();
+  }
+
+  save(callback, method)
+  {
+    var editor = this.getEditor();
+
+    if (method === undefined || method === null)
+      method = this.saveMethod;
+
+    var data = editor.getValue();
+
+    if (this.srcType === Editor.BASE64)
+    {
+//      console.log("encoding text: "+data);
+      data = CryptoJS.enc.Utf8.parse(data);
+      data = CryptoJS.enc.Base64.stringify(data);
+//      console.log("encoded to: "+data);
+    }
+
+    if (typeof callback === "function")
+    {
+      var newdata = callback(data, this);
+      if (typeof newdata === "string")
+        data = newdata;
+    }
+
+    if (this.srcElement !== null)
+    {
+      $(this.srcElement).val(data);
+    }
+    else if (this.srcUrl !== null)
+    {
+      var ctype;
+      if (this.srcType === Editor.BASE64)
+      {
+        ctype = 'text/base64';
+      }
+      else if (this.lang === 'xml')
+      {
+        ctype = 'application/xml';
+      }
+      else if (this.lang === 'json')
+      {
+        ctype = 'application/json';
+      }
+      else
+      {
+        ctype = 'text/plain';
+      }
+
+      var ajaxopts =
+      {
+        contentType: ctype,
+        data:        data,
+        method:      method,
+        processData: false,
+      };
+
+      return $.ajax(this.srcUrl, ajaxopts);
+    }
+    else
+    {
+      return data;
+    }
+  }
+
+}
+
+// The base64 value.
+Editor.BASE64 = 'base64';
+
