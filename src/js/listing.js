461036c2e2b9f50d2e8b4d88a54817b1da3a8da7
Timothy Totten <2010@huri.net>
2016-02-09 11:55:15 -0800

    Listing has better handling of null values during sort.

--- src/js/listing.js
+++ src/js/listing.js
@@ -330,12 +330,32 @@ Nano.Listing.prototype.refresh = function ()
       }
       var sort_other_asc = function (a, b)
       {
+        if (a[col] === undefined || a[col] === null)
+        {
+          if (b[col] === undefined || b[col] === null)
+            return 0;
+          return -1;
+        }
+        if (b[col] === undefined || b[col] === null)
+        {
+          return 1;
+        }
         if (a[col] < b[col]) return -1;
         if (a[col] > b[col]) return 1;
         return 0;
       }
       var sort_other_desc = function (a, b)
       {
+        if (a[col] === undefined || a[col] === null)
+        {
+          if (b[col] === undefined || b[col] === null)
+            return 0;
+          return 1;
+        }
+        if (b[col] === undefined || b[col] === null)
+        {
+          return -1;
+        }
         if (a[col] > b[col]) return -1;
         if (a[col] < b[col]) return 1;
         return 0;
