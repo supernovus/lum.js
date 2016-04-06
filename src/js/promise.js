b922eae58b7e2945aa122b224dd1fc795fa0dc23
Timothy Totten <2010@huri.net>
2016-04-06 10:10:14 -0700

    Promise updated to be more flexible.

--- src/js/promise.js
+++ src/js/promise.js
@@ -1,5 +1,5 @@
 /**
- * A promise interface for older browsers.
+ * A Promise interface that provides the jQuery Deferred API.
  */
 
 (function($, observable)
@@ -12,24 +12,96 @@
     return;
   }
 
-  if (observable === undefined)
+  /**
+   * Build the Promise object.
+   *
+   * @param boolean  deferred  Use the jQuery Deferred as base class?
+   *                           If true, the Promise will be derived from
+   *                           the jQuery Deferred class, and offer it's API.
+   *                           If false or omitted, the Promise will have
+   *                           an extremely simple API based on an example
+   *                           from the Riot.js v1 example application.
+   *                           In either case, 'done, 'fail', and 'always'
+   *                           methods will be available to send handlers to.
+   */
+  Nano.Promise = function (deferred)
   {
-    console.log("fatal error: No observable method found");
+    if (deferred)
+    {
+      var def = $.Deferred();
+      for (var f in def)
+      {
+        this[f] = def[f];
+      }
+    }
+    else
+    {
+      if (observable === undefined)
+      {
+        console.log("fatal error: No observable method found");
+        return;
+      }
+      var self = observable(this);
+      $.map(['done', 'fail', 'always'], function(name) 
+      {
+        self[name] = function(arg) 
+        {
+          return self[$.isFunction(arg) ? 'on' : 'trigger'](name, arg);
+        };
+      });
+    }
   }
 
   /**
-   * Promise interface, from Riot.js example app.
+   * Execute a delayed 'done' action.
+   *
+   * @param mixed  obj      The object to send to the done().
+   * @param str    ts       The textStatus to send to done().
+   * @param int    timeout  The timeout (in ms) defaults to 5.
    */
-  Nano.Promise = function ()
+  Nano.Promise.prototype.deferDone = function (obj, ts, timeout)
   {
-    var self = observable(this);
-    $.map(['done', 'fail', 'always'], function(name) 
-    {
-      self[name] = function(arg) 
+    var self = this;
+    if (timeout === undefined)
+      timeout = 5; // 5ms should be enough time to register .done events.
+    self.doneTimer = setTimeout(function() 
+    { 
+      if (typeof self.resolve === 'function')
       {
-        return self[$.isFunction(arg) ? 'on' : 'trigger'](name, arg);
-      };
-    });
+        self.resolve(obj, ts, self);
+      }
+      else
+      {
+        self.always(obj, ts, self);
+        self.done(obj, ts, self); 
+      }
+    }, timeout);
+  }
+
+  /**
+   * Execute a delayed 'fail' action.
+   *
+   * @param mixed  error    The message, code, or object to send to fail().
+   * @param str    ts       The textStatus to send to fail().
+   * @param int    timeout  The timeout (in ms) defaults to 5.
+   */
+  Nano.Promise.prototype.deferFail = function (error, ts, timeout)
+  {
+    var self = this;
+    if (timeout === undefined)
+      timeout = 5;
+    self.failTimer = setTimeout(function () 
+    {
+      if (typeof self.reject === 'function')
+      {
+        self.reject(self, ts, error);
+      }
+      else
+      {
+        self.always(self, ts, error);
+        self.fail(self, ts, error); 
+      }
+    }, timeout);
   }
 
 })(
